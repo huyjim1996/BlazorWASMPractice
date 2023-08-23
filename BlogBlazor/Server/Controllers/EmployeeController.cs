29320c97328f967ff25ab187dd73c06c8e54fe12
huyjim1996 <huyjim1996@gmail.com>
2023-08-23 16:58:02 +0700

    8/23/2023

---
+++ BlogBlazor/Server/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using BlogBlazor.Shared.Data;
+using BlogBlazor.Shared.ModelEF;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlogBlazor.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly MasterContext _context;
+        public EmployeeController(MasterContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<Employee>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployee()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+       
+    }
+}
